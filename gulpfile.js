85743a4d646aabcdd61a2fb35d9de20157205824
daniherr <daniherr@bancolombia.com.co>
2021-08-18 18:02:21 -0500

    subiendo papa con yuca más un poco de arroz

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var version= require('./package.json');
+
+//npm install gulp --save --only=dev
+gulp.task('try', function() {
+  console.log('Hola todo anda bien ');
+});
+
+//npm install gulp-sass
+gulp.task('sass', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+//npm install gulp-concat
+
+gulp.task('style', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(concat('style_main.css'))
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+
+//npm install gulp-minify-css
+gulp.task('style_min', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(minifyCSS())
+    .pipe(concat('style_main_'+version.version+'.min.css'))
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+//gulp watch
+gulp.task('watch', ['style_min'], function (){
+  gulp.watch('scss/**/*.scss', ['style_min']);
+});
